f98a2ba76b21298cc3fc7b070b139ab327344e46
MertcanYildirim078 <99068314@mydavinci.nl>
2023-06-12 12:16:36 +0200

    Gamepicker done

--- src-Gamecatalog/Game-Picker.js
+++ src-Gamecatalog/Game-Picker.js
@@ -2,11 +2,16 @@ var overzichtScherm = document.getElementById("overzicht");
 var winkelmandjeScherm = document.getElementById("winkelmandje");
 var switchButton = document.getElementById("switchButton");
 
-var priceFilterButton = document.getElementById("priceFilterButton")
-var priceFilterInput = document.getElementById("priceFilterInput")
+var priceFilterButton = document.getElementById("priceFilterButton");
+var priceFilterInput = document.getElementById("priceFilterInput");
 
+const ratingDrop = document.getElementById('rating')
+const genreDrop = document.getElementById('genre');
+
+ratingDrop.addEventListener('change', filterRating);
+genreDrop.addEventListener('change', filterGenre);
 switchButton.addEventListener("click", switchScreens);
-priceFilterButton.addEventListener("click", filterPrice)
+priceFilterButton.addEventListener("click", filterPrice);
 
 var winkelmand = [];
 
@@ -39,8 +44,6 @@ function addToCart() {
     console.log(this.dataset.name);
     }
 }
-
-
 
 function renderWinkelmandjeContent() {
 
@@ -76,29 +79,55 @@ function filterPrice() {
    }
 }
 
+function filterGenre() {
+   var genreValue = genreDrop.value;
+
+   var filteredList = games.filter((game) => (game.genre) == genreValue); {
+        console.log(filteredList);
+        renderGames(filteredList);
+    }
+}
+
+function filterRating() {
+    var ratingValue = ratingDrop.value;
+
+   var filteredList = games.filter((game) => (game.rating) == ratingValue); {
+        console.log(filteredList);
+        renderGames(filteredList);
+    }
+}
+
 function renderGames(gamesToRender) {
     overzichtScherm.innerHTML = "<h1>Dit is het scherm voor de overzicht</h1>";
-
 
     gamesToRender.forEach((game)=>{
         //maak element
         var gameBox = document.createElement("div");
+        var genreElem = document.createElement("h2");
         gameBox.classList.add("gameBoxStyle");
         var titelElem = document.createElement("h2");
+        var ratingElem = document.createElement("h2");
         var selectGameButton = document.createElement('input')
     
         selectGameButton.type = "checkbox"
         //put content
         titelElem.innerText = game.title;
         gameBox.innerText = game.price;
-    
+        genreElem.innerText = "Genre - " + game.genre;
+        ratingElem.innerText = "Rating - " + game.rating;
+
         selectGameButton.dataset.price = game.price;
         selectGameButton.dataset.name = game.title;
-    
+        selectGameButton.dataset.genre = game.genre;
+        selectGameButton.dataset.rating = game.rating;
+        
+
         selectGameButton.addEventListener("click", addToCart)
         //scherm aantonen
         gameBox.appendChild(selectGameButton);
         gameBox.appendChild(titelElem);
+        gameBox.appendChild(genreElem);
+        gameBox.appendChild(ratingElem);
         overzichtScherm.appendChild(gameBox);
     })    
 }
